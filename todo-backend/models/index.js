dc41083d54701c54710b0bf71757ed24c5313bc5
nist <nist.dev@yandex.ru>
2023-12-29 14:06:57 +0300

    переделал backend и добавил frontend

--- todo-backend/models/index.js
+++ todo-backend/models/index.js
@@ -1,10 +1,9 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/todoDB',{
-    keepAlive: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-mongoose.set('debug', true);
-mongoose.Promise = Promise;
-
-module.exports.Todo = require("./todo");
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://localhost/todo-app", {
+  // connecting to the mongodb database name: "todo-app" locally
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+mongoose.set("debug", true) // enabling debugging information to be printed to the console for debugging purposes
+mongoose.Promise = Promise // setting mongoose's Promise to use Node's Promise
+module.exports.Todo = require("./todo") // requiring the todo model that we just created in mongodb
--- todo-backend/models/todo.js
+++ todo-backend/models/todo.js
@@ -1,17 +1,20 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose") // requiring the mongoose package
 
 const todoSchema = new mongoose.Schema({
-    task: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    completed:{
-        type: Boolean,
-        default: false
-    }
-});
+  // creating a schema for todo
+  task: {
+    // field1: task
+    type: String, // task is a string
+    unique: true, // it has to be unique
+    required: true, // it is required
+  },
+  completed: {
+    // field2: completed
+    type: Boolean, // it is a boolean
+    default: false, // the default is false
+  },
+})
 
-const todoModel  = mongoose.model("Todo", todoSchema);
+const todoModel = mongoose.model("Todo", todoSchema) // creating the model from the schema
 
-module.exports = todoModel;
+module.exports = todoModel // exporting the model
